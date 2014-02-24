3b7a860d9cee882d246752d480455a42e47d9f35
Sinuhé Jaime Valencia <sjaime@consistent.com.mx>
2014-02-24 12:32:49 -0600

    Fixed style and use of cout and cin instead of printf and scanf on aleatorios. Style on matriz_triangular Fixed typos on comments

--- task/aleatorios.cpp
+++ task/aleatorios.cpp
@@ -1,44 +1,61 @@
+/*
+ * Author: Eduardo Espinosa Serrano
+ *
+ * LastUpdated: Sier Violencia <sierisimo@gmail.com>    24-Feb-2014
+ * 
+ * URL: https://github.com/hoffman775/programming/blob/master/task/aleatorios.cpp
+ *
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream> /*contine todo lo orientado a obejos*/
 #include <math.h>
-using namespace std;
-void cabecera(void); /*Funcion prototipo */
-void cabecera2(void); /*Funcion prototipo */
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+void cabecera(void); //Funcion prototipo 
+void cabecera2(void); //Funcion prototipo 
+
 int main(void)
 {
     time_t comienza,final;
     comienza = time(NULL);
     int x;
     char resp;
-    srand((unsigned) time(NULL)); /*inicialidor de nuemros aleatorios*/
+    srand((unsigned) time(NULL)); //inicialidor de nuemros aleatorios
     do {
         cabecera();
-        x = rand();   /*el numero aleatorio generado se asigna a la variable x */
-        printf("\nEl numero pseudo aleatorio generado es : %d",x);
-        if (pow(-1,x) > 0)
-            
-            printf(" y es PAR");
+        x = rand(); //el numero aleatorio generado se asigna a la variable x 
+        cout<<"\nEl numero pseudo aleatorio generado es : "<<x;
+        if (pow(-1,x) > 0)    
+            cout<<" y es PAR";
         else{
-             printf(" y es IMPAR");
-             }    
-        printf("\n\n\n¿Deseas continuar generando numeros pseudos  aleatorios?\n");
-        printf("\n [S] or [N]\n");
+             cout<<" y es IMPAR";
+        }    
+        cout<<"\n\n\n¿Deseas continuar generando numeros pseudos  aleatorios?"<<endl;
+        cout<<"\n [S] or [N]"<<endl;
         
-        scanf("%s",&resp);
+        cin>>resp;
+
         system("cls");
-        }
-    while (resp=='s' || resp=='S');
+    } while (resp=='s' || resp=='S');
+    
     final= time(NULL);
+    
     cabecera2();
     printf("\n\nTiempo de ejecucion  %2f SEG\n",difftime(final,comienza));
+    
     system("pause");
-
+    
+    return 0;
 }
-/* finciones */
-void cabecera(void)
 
- {
+/* funciones */
+void cabecera(void)
+{
     printf("\n\t\t*************************************************\n");
     printf("\t\t*                                               *\n");
     printf("\t\t*          UNAM - FES ACATLAN MAC               *\n");
@@ -46,15 +63,14 @@ void cabecera(void)
     printf("\t\t*        y determina si es PAR o IMPAR          *\n");
     printf("\t\t*                                               *\n");
     printf("\t\t*************************************************\n");
- }
-void cabecera2(void)
+}
 
- {
+void cabecera2(void)
+{
     printf("\n\t\t*************************************************\n");
     printf("\t\t*                                               *\n");
     printf("\t\t*   Hecho por:                                  *\n");
     printf("\t\t*         Eduardo Espinoza Serrano              *\n");
     printf("\t\t*                                               *\n");
     printf("\t\t*************************************************\n");
- }
-
+}
--- task/matriz_triangular.cpp
+++ task/matriz_triangular.cpp
@@ -1,78 +1,84 @@
-//  WINDOWS
+/*
+ * Author: Eduardo Espinosa Serrano
+ *
+ * LastUpdate: Sier Violencia <sierisimo@gmail.com> 24-Feb-2014
+ * 
+ * URL: https://github.com/hoffman775/programming/blob/master/task/matriz_triangular.cpp
+ */
+
 #include <stdio.h>
 #include <stdlib.h> //incluye srand() y rand()
-#include <conio.h>
 #include <time.h> //incluye time()
+
 #define MAX 10
-#include <iostream>
+
 int mat[MAX][MAX],a,b; //tamaño de la matriz  
+
 void cabecera(void);
 void cabecera2(void);
+
 char resp;
+
 int main()
 {
-time_t comienza,final;
-comienza = time(NULL);
-   system("cls");
+    time_t comienza,final;
+    comienza = time(NULL);
+    system("cls");
     
-  do
-  {  
-   cabecera();
-   printf("\nCuantas columnas quieres:  \t");
-   scanf("%d", &a);
-   printf("Cuantos renglones quieres: \t");
-   scanf("%d", &b);
-   printf("\n\n");
+    do
+    {  
+        cabecera();
+        printf("\nCuantas columnas quieres:  \t");
+        scanf("%d", &a);
+        printf("Cuantos renglones quieres: \t");
+        scanf("%d", &b);
+        printf("\n\n");
        
-       if (a==b)
-         {
-          srand(time(0));//numeros Aleatorios en funcion del tiempo
+        if (a==b)
+        {
+            srand(time(0));//numeros Aleatorios en funcion del tiempo
             for (int i=0;i<a;i++)//para desplazarse por las columnas
-             {
+            {
                 for (int j=0;j<b;j++)//para desplazarse por las filas
-                 {
+                {
                     if (i>=j)
-                        { mat[i][j]=rand();//Agrega numero aleatorio a la posicion ij de la matriz
-                           printf("\t%d",mat[i][j]);//imprime elemento de la matriz en pantalla
-                         }
+                    {
+                        mat[i][j]=rand();//Agrega numero aleatorio a la posicion ij de la matriz
+                        printf("\t%d",mat[i][j]);//imprime elemento de la matriz en pantalla
+                    }
                     else
-                     {
-                       printf("\t0");
-                     }
-            
-                  }
-          printf("\n\n");//para dejar espacios entre filas.
-              }
-          
-         }
-       else
-           {
-           system("cls");
-           printf("Para obtener la matriz triangular, esta tiene que ser una matriz cuadrada.\n");
-           printf("La matriz cuadrada tiene el mismo numero de filas que de columnas.\n\n");
-           
-           }
-
-    printf("\n\n\n¿Deseas continuar generando otra matriz con numeros pseudos  aleatorios?\n");
-    printf("\n [S] or [N]\n");
+                        printf("\t0");
+                }
+                printf("\n\n");//para dejar espacios entre filas.
+            } 
+        }
+        else
+        {
+            system("cls");
+            printf("Para obtener la matriz triangular, esta tiene que ser una matriz cuadrada.\n");
+            printf("La matriz cuadrada tiene el mismo numero de filas que de columnas.\n\n");
+        }
+        printf("\n\n\n¿Deseas continuar generando otra matriz con numeros pseudos  aleatorios?\n");
+        printf("\n [S] or [N]\n");
         
         scanf("%s",&resp);
-    system("cls");
-}  
-  while (resp=='s' || resp=='S');
+        system("cls");
+    } while (resp=='s' || resp=='S');
+    
     final= time(NULL);
+    
     system("cls");
+    
     cabecera2();
+    
     printf("\n\nTiempo de ejecucion  %2f SEG\n",difftime(final,comienza));
 
-   system("pause");
-   
-
+    system("pause");
+    return 0;
 }
 
 void cabecera(void)
-
- {
+{
     printf("\n\t\t*************************************************\n");
     printf("\t\t*                                               *\n");
     printf("\t\t*          UNAM  FES - ACATLAN MAC              *\n");
@@ -81,15 +87,14 @@ void cabecera(void)
     printf("\t\t*  de la misma.                                 *\n");
     printf("\t\t*                                               *\n");
     printf("\t\t*************************************************\n");
- }
+}
 
 void cabecera2(void)
-
- {
+{
     printf("\n\t\t*************************************************\n");
     printf("\t\t*                                               *\n");
     printf("\t\t*   Hecho por:                                  *\n");
     printf("\t\t*         Eduardo Espinoza Serrano              *\n");
     printf("\t\t*                                               *\n");
     printf("\t\t*************************************************\n");
- }
+}
